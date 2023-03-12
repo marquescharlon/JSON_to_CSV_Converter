99f02015cc5c5018b354f2e8f84b979d72559102
Marquescharlon Santos <marquescharlonrs@gmail.com>
2023-03-12 00:06:09 -0300

    3. Como ler a informação JSON para realizar a manipulação?

--- AppConverterConsole/Program.cs
+++ AppConverterConsole/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// Pedir que seja informado na execução do programa
+//var textoJSON = Console.ReadLine();
+
+// 3. Como ler a informação JSON para realizar a manipulação?
+var textoJSON = @"{""Vaga"":""Desenvolvedor.NET"",""Empresa"":""Yardim"",""Nivel"": ""Junior"",""Site"": ""https://www.yardim.com.br/""}";
+Console.WriteLine(textoJSON);
