96f31736612fa515d35ba367b81f8b6b78f074ea
Marquescharlon Santos <marquescharlonrs@gmail.com>
2023-03-12 01:44:20 -0300

    4. Como manipular cada objeto encontrado do JSON?

--- AppConverterConsole/Program.cs
+++ AppConverterConsole/Program.cs
@@ -4,5 +4,16 @@
 //var textoJSON = Console.ReadLine();
 
 // 3. Como ler a informação JSON para realizar a manipulação?
-var textoJSON = @"{""Vaga"":""Desenvolvedor.NET"",""Empresa"":""Yardim"",""Nivel"": ""Junior"",""Site"": ""https://www.yardim.com.br/""}";
+
+var textoJSON = @"{""Vaga"":""Desenvolvedor.NET"",""Empresa"":""Yardim"",""Nivel"":""Junior"",""Site"":""https://www.yardim.com.br/""}";
 Console.WriteLine(textoJSON);
+
+// 4. Como manipular cada objeto encontrado do JSON?
+
+var textoSTRING = Convert.ToString(textoJSON).Replace("{", "").Replace("}", "").Replace("\"", "").Trim();
+var linha = textoSTRING.Split(",");
+
+foreach (var item in linha)
+{
+    Console.WriteLine(item);
+}
