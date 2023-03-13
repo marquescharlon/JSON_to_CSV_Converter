99e833e4f6af607206337480202da9d9f058d067
Marquescharlon Santos <marquescharlonrs@gmail.com>
2023-03-12 22:30:53 -0300

    6.a. Construir primeiro o cabeçalho do CSV

--- AppConverterConsole/Program.cs
+++ AppConverterConsole/Program.cs
@@ -31,13 +31,15 @@ Console.Write("\n");
 
 Console.WriteLine("5. Como extrair as Chaves que irão formar o cabeçalho no formato CSV?");
 
-ArrayList campos = new ArrayList();
+string[] campos = new string[textoSTRING.Length];
 var primeiralinha = textoSTRING.Split(",");
 
+int p = 0;
 foreach (var item in primeiralinha)
 {
-    var vagas = item.Split('"');
-    campos.Add(vagas[1]);
+    var nomes = item.Split('"');
+    campos.SetValue(nomes[1], p);
+    p++;
 }
 
 Console.WriteLine(campos[0]);
@@ -46,3 +48,23 @@ Console.WriteLine(campos[2]);
 Console.WriteLine(campos[3]);
 
 Console.Write("\n");
+
+// 6. Como construir meu CSV?
+
+// a. Construir primeiro o cabeçalho do CSV
+
+string[] textoCSV = new string[campos.Length];
+
+
+foreach (var item in campos)
+{
+    if (item != null)
+    {
+        textoCSV[0] += "\"" + item + "\",";
+    }
+}
+
+Console.WriteLine(textoCSV[0]);
+Console.WriteLine(textoCSV[1]);
+Console.WriteLine(textoCSV[2]);
+Console.WriteLine(textoCSV[3]);
