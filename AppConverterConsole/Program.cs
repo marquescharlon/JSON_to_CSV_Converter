53f1a58dce845c120afb7ac6bf457b6d4cecbfe8
Marquescharlon Santos <marquescharlonrs@gmail.com>
2023-03-12 21:30:23 -0300

    5. Como extrair as Chaves que irão formar o cabeçalho no formato CSV?

--- AppConverterConsole/Program.cs
+++ AppConverterConsole/Program.cs
@@ -1,19 +1,48 @@
-﻿Console.Clear();
+﻿using System.Collections;
+
+Console.Clear();
 
 // Pedir que seja informado na execução do programa
 //var textoJSON = Console.ReadLine();
 
 // 3. Como ler a informação JSON para realizar a manipulação?
 
+Console.WriteLine("3. Como ler a informação JSON para realizar a manipulação?");
+
 var textoJSON = @"{""Vaga"":""Desenvolvedor.NET"",""Empresa"":""Yardim"",""Nivel"":""Junior"",""Site"":""https://www.yardim.com.br/""}";
 Console.WriteLine(textoJSON);
+Console.Write("\n");
 
 // 4. Como manipular cada objeto encontrado do JSON?
 
-var textoSTRING = Convert.ToString(textoJSON).Replace("{", "").Replace("}", "").Replace("\"", "").Trim();
+Console.WriteLine("4. Como manipular cada objeto encontrado do JSON?");
+
+var textoSTRING = Convert.ToString(textoJSON).Replace("{", "").Replace("}", "").Trim();
 var linha = textoSTRING.Split(",");
 
 foreach (var item in linha)
 {
     Console.WriteLine(item);
 }
+
+Console.Write("\n");
+
+// 5. Como extrair as Chaves que irão formar o cabeçalho no formato CSV?
+
+Console.WriteLine("5. Como extrair as Chaves que irão formar o cabeçalho no formato CSV?");
+
+ArrayList campos = new ArrayList();
+var primeiralinha = textoSTRING.Split(",");
+
+foreach (var item in primeiralinha)
+{
+    var vagas = item.Split('"');
+    campos.Add(vagas[1]);
+}
+
+Console.WriteLine(campos[0]);
+Console.WriteLine(campos[1]);
+Console.WriteLine(campos[2]);
+Console.WriteLine(campos[3]);
+
+Console.Write("\n");
